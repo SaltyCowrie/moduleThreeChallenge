d0501a55523d7533af7734c7e82ea6925ec5c9f5
Nymph <trismageefl@gmail.com>
2024-01-18 15:58:39 -0500

    Improved the README section

--- Assets/script.js
+++ Assets/script.js
@@ -3,12 +3,14 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  event.preventDefault()
+  var password = generatePassword(hasUpper, hasLower, hasNumbers);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function() {
+  return writePassword();
+});
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# moduleThreeChallenge
+# Description
+
 Working with Javascript to learn a more in-depth understand of logic based coding
+
+## Screenshot
+
+<img width="1470" alt="Screenshot 2024-01-18 at 3 20 35 PM" src="https://github.com/SaltyCowrie/moduleThreeChallenge/assets/150820140/45805b3f-2b1e-4517-8f95-a63a607dcc9e">
+
+
+## Link
+
+https://saltycowrie.github.io/moduleThreeChallenge/
+
+## Credit
+
+N/A 
+
+## License 
+
+See MIT license in repo
