9c90f4dd1fe1daf9f297f714f8f470afc85f87ff
Nymph <trismageefl@gmail.com>
2024-01-21 10:56:01 -0500

    Further additions to JavaScript

--- Assets/script.js
+++ Assets/script.js
@@ -3,19 +3,24 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var hasLowercase = 
-  var hasUppercase = 
-  var hasNumbers = 
-  var hasSpecialcharacters = 
+  var num = (1,2);
+  var hasUpper = ("A", "Z");
+  var lower = ("a","z");
+  var special = ("$","*");
   var password = generatePassword(
-    var password = ()
-    var userCharacters = ()
+    var password = ();
+    var userCharacters = ();
+    var length = window.prompt("How many characters do you want between 8 and 128?");
+    if (
+      userChoice = true;
+    )
   );
-  var length = prompt()
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-}
+
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", function() {
