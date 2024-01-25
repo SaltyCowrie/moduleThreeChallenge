d8138264ab1611e4620845b545e7cee0d35457d6
Nymph <trismageefl@gmail.com>
2024-01-25 16:03:49 -0500

    Slowly but surely making more sense

--- Assets/script.js
+++ Assets/script.js
@@ -3,24 +3,38 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var num = (1,2);
-  var hasUpper = ("A", "Z");
-  var hasLower = ("a","z");
-  var special = ("$","*");
-  var password = generatePassword(
-    userChoice = (num, hasUpper, hasLower, special)
-  );
-  var length = window.prompt('How many characters would you like between 8 and 128?');
+  var num = [1,2,3,4,5,6,7,8,9];
+  var hasUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var hasLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var hasSpecial = ["$","*","&","%","@"];
+  var userChoice = ();
+  var password = generatePassword( function() {
 
-  if {
-    (length >= 8, correct: true);
-    (length <= 128, correct: true);
-  };
+    let num = prompt("are numbers ok to use?")
+    if { 
 
-  if else {
-    (length = false);
-    window.prompt ('Adjust value');
-  };
+    } else { 
+
+    };
+    let hasUpper = prompt("are uppercase letters ok?")
+    if {
+
+     } else { 
+
+     };
+    let hasLower = prompt("are lower case letters ok?")
+    if { 
+
+    } else { 
+
+    };
+    let hasSpecial = prompt("are special characters ok?")
+    if {
+
+     } else {
+
+      };
+  });
 
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
@@ -29,6 +43,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", function() {
-  writePassword()
-  return;
+  writePassword();
 });
