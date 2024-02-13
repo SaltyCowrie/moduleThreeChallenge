5409409c07b13edc76d89d7515121c25f31f29de
Nymph <trismageefl@gmail.com>
2024-02-12 19:38:46 -0500

    almost there

--- Assets/script.js
+++ Assets/script.js
@@ -1,46 +1,30 @@
 var generateBtn = document.querySelector("#generate");
 
+// variables
+var passwordLength = 8;
+var includeUpper = true;
+var includeLower = true;
+var includeSpecial = true;
+var includeNumbers = true;
+
 // Write password to the #password input
 function writePassword() {
-  var num = [1,2,3,4,5,6,7,8,9];
+
+  var hasNumbers = [0,1,2,3,4,5,6,7,8];
   var hasUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   var hasLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var hasSpecial = ["$","*","&","%","@"];
-  var userChoice = ();
-  var password = generatePassword( function() {
 
-    let num = prompt("are numbers ok to use?")
-    if { 
-      userChoice: true;
-    } else if { 
-      userChoice: false;
-    };
-    let hasUpper = prompt("are uppercase letters ok?")
-    if {
-      userChoice: true;
-     } else if { 
-      userChoice: false;
-     };
-    let hasLower = prompt("are lower case letters ok?")
-    if { 
-      userChoice: true;
-    } else if{ 
-      userChoice: false;
-    };
-    let hasSpecial = prompt("are special characters ok?")
-    if {
-      userChoice: true;
-     } else if {
-      userChoice: false;
-      };
-  });
-
+  var password = generatePassword(hasLower, hasNumbers, hasSpecial, hasUpper);
   var passwordText = document.querySelector("#password");
+  
   passwordText.value = password;
-
 };
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", function() {
   writePassword();
+  return;
 });
