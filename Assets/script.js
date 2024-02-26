724b3293b9a27fa820229dac98d2df469954a0d4
Nymph <trismageefl@gmail.com>
2024-02-26 15:29:42 -0500

    Updates to Java code

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,13 @@
 var generateBtn = document.querySelector("#generate");
 
+var hasNumbers = [0,1,2,3,4,5,6,7,8];
+var hasUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var hasLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var hasSpecial = ["$","*","&","%","@"];
+
+
 // Write password to the #password input
 function writePassword() {
-
-  var hasNumbers = [0,1,2,3,4,5,6,7,8];
-  var hasUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  var hasLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var hasSpecial = ["$","*","&","%","@"];
 
   var password = generatePass();
   var passwordText = document.querySelector("#password");
@@ -19,7 +20,6 @@ function writePassword() {
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", function() {
-  writePassword();
-  return;
+generateBtn.addEventListener("click", function{
+  console.log(writePassword)
 });
