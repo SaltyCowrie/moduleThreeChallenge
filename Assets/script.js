82fba2ae01d2ab89fc7444853c99cce2dd22e11c
Nymph <trismageefl@gmail.com>
2024-02-27 21:01:10 -0500

    Creating confirm alerts to verify user input

--- Assets/script.js
+++ Assets/script.js
@@ -1,25 +1,64 @@
 var generateBtn = document.querySelector("#generate");
 
-var hasNumbers = [0,1,2,3,4,5,6,7,8];
-var hasUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var hasLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var hasSpecial = ["$","*","&","%","@"];
 
 
-// Write password to the #password input
-function writePassword() {
+// Creates user input
+function generatePassword() {
 
-  var password = generatePass();
-  var passwordText = document.querySelector("#password");
+  var hasNumbers = [0,1,2,3,4,5,6,7,8];
+  var hasUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var hasLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var hasSpecial = ["$","*","&","%","@"];
+  var hasLength = [5];
   
-  passwordText.value = password;
+  var length = confirm("Is 5 characters okay?")
+  if (length === false) {
+    return "Goodbye";
+  }
 
+  var numbers = confirm("Are numbers okay?")
+  if (numbers === false) {
+    return "Goodbye";
+  }
+
+  var upper = confirm("Are uppercase characters okay?")
+  if (upper === false) {
+    return "Goodbye";
+  }
+
+  var lower = confirm("Are lowercase characters okay?")
+  if (lower === false) {
+    return "Goodbye";
+  }
+  
+  var special = confirm("Are special characters okay?")
+  if (special === false) {
+    return "Goodbye";
+  }
 
 };
 
 
+// Runs button to input text
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", function{
-  console.log(writePassword)
-});
+
+// Event listener
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
